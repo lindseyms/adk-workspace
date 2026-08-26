4b1405dbc4a5e81ddc7f547c811d7c33a42d8470
Lindsey Schaefer <lindseygatie@gmail.com>
2026-08-26 11:28:51 -0500

    Update model parameter for root_agent to 'gemini-2.5-flash'

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='<FILL_IN_MODEL>',
+    model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
